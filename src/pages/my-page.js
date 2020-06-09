c2199fa488a7c76fbbcbd51acf938a80fd664c56
Laura <laura.stasiule@gmail.com>
2020-06-09 13:47:11 +0300

    Add all files of blog code for gatsby setup

---
+++ src/pages/my-page.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const myPage = () => (
+    <Layout>
+        <h1>Howdy</h1>
+        <p>Welcome to my page</p>
+        <Link to="/">Go back to the homepage</Link>
+    </Layout>
+)
+
+export default myPage
